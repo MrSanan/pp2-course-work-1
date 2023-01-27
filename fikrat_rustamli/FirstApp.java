f1b41b273fd667e88ae87bdc7c9066b1ff09f508
Fikrat Rustamli 16058 <frustamli16058@ada.edu.az>
2023-01-27 15:48:12 +0400

    Add work 01 Practical

---
+++ fikrat_rustamli/FirstApp.java
@@ -0,0 +1,8 @@
+public class FirstApp {
+    public static void main (String args[]) {
+
+    System.out.println ("Welcome to our course");
+  
+    System.out.println ("Fikrat Rustamli");
+    }
+}  
---
+++ fikrat_rustamli/ThirdApp.java
@@ -0,0 +1,13 @@
+public class ThirdApp {
+    
+
+    public static void main (String args[]) {
+
+       var res=(float) 1/5 *2/3-3/10F;
+       
+       // System.out.println (res);
+        System.out.printf ("%2f",res);
+      
+        }
+
+}
